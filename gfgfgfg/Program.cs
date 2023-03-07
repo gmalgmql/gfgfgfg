e223f3cedfd8823310ecb5c5ca1b78da317668f2
HNU <HNU@DESKTOP-8C4Q3SL>
2023-03-07 14:31:29 +0900

    초기 커밋.

---
+++ gfgfgfg/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gfgfgfg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Write"); Console.Write("Write");
+            Console.Write("Write");
+         Console.Write("Write"); 
+         Console.Write("Write");
+        }
+    }
+}
